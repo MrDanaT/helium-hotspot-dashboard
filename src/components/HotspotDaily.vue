8b494ee87dd2bad1add1003bdbda571937f3bfad
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2021-09-13 19:32:58 +0200

    Add first dashboard version

---
+++ src/components/HotspotDaily.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <a-table
+      :columns="columns"
+      :data-source="hotspots"
+      :loading="loading"
+      :pagination="false"
+      :row-key="(hs) => hs.hotspotInfo.address"
+      bordered
+    >
+      <template slot="title"> Hotspot overview </template>
+      <span slot="name" slot-scope="name">
+        <a
+          v-bind:href="
+            'https://explorer.helium.com/hotspots/' + findAddress(name)
+          "
+          >{{ name }}</a
+        >
+      </span>
+      <span slot="status" slot-scope="status">
+        <a-tag :color="status === 'online' ? 'green' : 'volcano'">
+          {{ status }}
+        </a-tag>
+      </span>
+      <template slot="footer"> © Dana Inc. </template>
+    </a-table>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+    name: 'HotspotDaily'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
+import Antd from 'ant-design-vue';
 import App from "./App.vue";
 import "./registerServiceWorker";
+import "ant-design-vue/dist/antd.css";
 import store from "./store";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios, axios)
+Vue.use(Antd);
 
 new Vue({
   store,
