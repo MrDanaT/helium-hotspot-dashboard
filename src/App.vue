1f87311bb248db7e21d535ed3346122a1890ddfe
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2022-02-03 21:35:16 +0100

    Finish optimising table

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
+    <div id="banner">Helium Hotspot Dashboard</div>
     <a-row>
       <a-col
         ><a-card
           title="User input data"
-          style="width: 30%; border-width: 1px; border-color: black"
+          style="width: 100%; border-width: 1px; border-color: black"
         >
           <a-col>
             <a-row>
@@ -16,7 +17,7 @@
                   @change="datePicked"
                 />
                 <a-button @click="addAddress" type="primary"
-                  >Add all HS</a-button
+                  >Load data</a-button
                 ></a-form-model-item
               >
             </a-row>
@@ -29,9 +30,15 @@
               />
               <a-button
                 @click="addAddress"
-                :style="{ width: 100 - textBoxWidth + '%' }"
+                :style="{ width: (100 - textBoxWidth) / 2 + '%' }"
                 type="primary"
                 >Add HS</a-button
+              >
+              <a-button
+                @click="removeAddress(inputHotspotAddress)"
+                :style="{ width: (100 - textBoxWidth) / 2 + '%' }"
+                type="danger"
+                >Remove HS</a-button
               ></a-row
             >
             <br />
@@ -51,13 +58,24 @@
           </a-col>
         </a-card>
       </a-col>
-
       <a-col>
+        <a-card title="Statistics">
+          <a-card-grid type="inner">
+            Total hotspots: {{ hotspotDict.length }}
+          </a-card-grid>
+          <a-card-grid type="inner">
+            Last Days Total:
+            {{ totalPreviousDay.toFixed(5) }} HNT
+          </a-card-grid>
+          <a-card-grid type="inner">
+            Total online:
+            {{ hotspotDict.filter((hs) => hs.status === "online").length }}
+          </a-card-grid>
+        </a-card>
+        <br />
         <a-table
           :columns="columns"
           :data-source="mappedHotspots"
-          :row-key="(hs) => hs.address"
-          :name="counter"
           :pagination="true"
           bordered
         >
@@ -66,15 +84,21 @@
               v-bind:href="
                 'https://explorer.helium.com/hotspots/' + findAddress(name)
               "
-              >{{ name }}</a
+              >{{ normalise(name) }}</a
             >
           </span>
           <span slot="status" slot-scope="status">
             <a-tag :color="status === 'online' ? 'green' : 'volcano'">
               {{ status }}
             </a-tag>
-          </span></a-table
-        >
+          </span>
+          <span slot="options" slot-scope="skip, hs">
+            <a-button
+              type="danger"
+              @click="removeAddress(hs.address)"
+            ></a-button>
+          </span>
+        </a-table>
       </a-col>
     </a-row>
   </div>
@@ -96,22 +120,33 @@ const columns = [
   {
     title: "Reward Scale",
     dataIndex: "reward_scale",
+    ellipsis: true,
   },
   {
     title: "Status",
-    dataIndex: "online",
+    dataIndex: "status",
     scopedSlots: {
       customRender: "status",
     },
+    ellipsis: true,
   },
   {
     title: "Sync Percentage (%)",
     dataIndex: "syncPercentage",
+    ellipsis: true,
   },
   {
     title: "Last Day Earned (HNT)",
     dataIndex: "lastDayEarned",
     ellipsis: true,
+  },
+  {
+    title: "Options",
+    dataIndex: "options",
+    ellipsis: true,
+    scopedSlots: {
+      customRender: "options",
+    },
   },
 ];
 
@@ -122,27 +157,42 @@ export default {
     return {
       hotspotDict: [],
       hotspotAPIUrl: "https://api.helium.io/v1/hotspots",
-      startDate: moment(new Date() - 7),
+      startDate: moment(this.currDay).days(-3),
       inputHotspotAddress: "",
       inputOwnerAddress: "",
       textBoxWidth: 80,
       columns,
-      counter: 0,
     };
   },
   computed: {
     mappedHotspots() {
       return this.hotspotDict;
     },
+    totalPreviousDay() {
+      return this.hotspotDict
+        .map((hs) => hs.lastDayEarned)
+        .reduce(
+          (previousValue, currentValue) => previousValue + currentValue,
+          0
+        );
+    },
+    currDay() {
+      return new Date(new Date().toISOString());
+    },
   },
   methods: {
     findIndexOf(nameOrAddress) {
-      console.log("FIND: ", nameOrAddress, "IN: ", this.hotspotDict);
       const idx = this.hotspotDict.findIndex(
         (hs) => hs.name === nameOrAddress || hs.address === nameOrAddress
       );
 
       return idx;
+    },
+    normalise(name) {
+      return name
+        .split("-")
+        .join(" ")
+        .replace(/(^\w{1})|(\s{1}\w{1})/g, (match) => match.toUpperCase());
     },
     hotspotAlreadyAdded(nameOrAddress) {
       let idx = this.findIndexOf(nameOrAddress);
@@ -191,22 +241,30 @@ export default {
     disabledDate(current) {
       return current > moment();
     },
-    convertAPIHotspotToMyHotspot(data) {
+    convertAPIHotspotToMyHotspot(data, owner) {
       return {
-        online: data.status.online,
+        key: data.address,
+        status: data.status.online,
         height: data.status.height || 0,
         reward_scale: data?.reward_scale?.toFixed(2) || 0,
         name: data.name,
         syncPercentage: ((data?.status?.height / data.block) * 100).toFixed(2),
         address: data.address,
+        lastDayEarned: this.getLastDayEarned(data.address),
+        owner: owner,
       };
+    },
+    getLastDayEarned(address) {
+      this.getYesterdaysReward(address).then(({ data }) => {
+        this.hotspotDict[this.findIndexOf(address)].lastDayEarned = data.total;
+      });
     },
     async processHotspot(hotspotAddress) {
       await Vue.axios
         .get(`${this.hotspotAPIUrl}/${hotspotAddress}`)
         .then(({ data }) => {
           data = data.data;
-          this.hotspotDict.push(this.convertAPIHotspotToMyHotspot(data));
+          this.hotspotDict.push(this.convertAPIHotspotToMyHotspot(data, null));
         });
     },
     openNotification(message, description, type) {
@@ -215,42 +273,26 @@ export default {
         description,
       });
     },
-    removeFromHotspots() {
-      let idx = this.hotspotNames.findIndex(
-        (hs) => hs === this.inputHotspotAddress
-      );
-      if (idx < 0)
-        idx = this.hotspotAddresses.findIndex(
-          (hs) => hs === this.inputHotspotAddress
-        );
-
-      if (idx < 0) return;
-
-      this.hotspotAddresses.splice(idx, 1);
-      this.inputHotspotAddress = "";
-    },
     async getOwnersHotspots() {
-      try {
-        let res = await Vue.axios({
-          url: `https://api.helium.io/v1/accounts/${this.inputOwnerAddress}/hotspots`,
-          method: "get",
-          timeout: 8000,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        if (res.status == 200) {
-          return res.data.data.map((hs) =>
-            this.convertAPIHotspotToMyHotspot(hs)
+      let results = [];
+      await Vue.axios
+        .get(
+          `https://api.helium.io/v1/accounts/${this.inputOwnerAddress}/hotspots`
+        )
+        .then(({ data }) => {
+          results = data.data.map((hs) =>
+            this.convertAPIHotspotToMyHotspot(hs, this.inputOwnerAddress)
           );
-        }
-      } catch (err) {
-        this.openNotification(
-          "Cannot add hotspot",
-          "Address is wrong.",
-          "error"
-        );
-      }
+        })
+        .catch(() => {
+          this.openNotification(
+            "Cannot add hotspot",
+            "Address is wrong.",
+            "error"
+          );
+        });
+
+      return results;
     },
     addOwnersHotspots() {
       let isAdded = 0;
@@ -258,6 +300,7 @@ export default {
       this.getOwnersHotspots()
         .then((arr) => {
           arr.forEach((hs) => {
+            console.log(hs);
             if (this.findIndexOf(hs.address) == -1) {
               this.hotspotDict.push(hs);
               this.inputOwnerAddress = "";
@@ -284,15 +327,73 @@ export default {
           }
         });
     },
-    removeOwnersHotspots() {
-      this.getOwnersHotspots().then((arr) => {
-        arr.forEach((element) => {
-          const idx = this.hotspotAddresses.findIndex((x) => x === element);
-          if (idx < 0) return;
-          this.hotspotAddresses.splice(idx, 1);
-          this.inputOwnerAddress = "";
+    getStartOfGivenDay(date) {
+      var res = new Date(date.getTime());
+      res.setUTCHours(0, 0, 0, 0);
+      return res;
+    },
+    getEndOfGivenDay(date) {
+      var res = new Date(date.getTime());
+      res.setUTCHours(23, 59, 59, 999);
+      return res;
+    },
+    async getYesterdaysReward(address) {
+      const now = this.currDay;
+      var yesterday = new Date(now.getTime());
+      yesterday.setDate(now.getDate() - 1);
+      return await this.getReward(
+        address,
+        this.getStartOfGivenDay(yesterday),
+        this.getEndOfGivenDay(yesterday)
+      );
+    },
+    async getTodaysReward(address) {
+      const startDay = this.getStartOfGivenDay(new Date());
+      const endDay = this.getEndOfGivenDay(startDay);
+      let result = await this.getReward(address, startDay, endDay);
+      return result;
+    },
+    async getReward(address, startDay, endDay) {
+      let result = await this.rewardCall(address, startDay, endDay);
+      return result;
+    },
+    async rewardCall(address, startDay, endDay) {
+      try {
+        let res = await Vue.axios({
+          url: `${
+            this.hotspotAPIUrl
+          }/${address}/rewards/sum?min_time=${startDay.toISOString()}&max_time=${endDay.toISOString()}`,
+          method: "get",
+          timeout: 8000,
+          headers: {
+            "Content-Type": "application/json",
+          },
         });
-      });
+
+        if (res.status == 200) {
+          return res.data;
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    removeAddress(address) {
+      const idx = this.findIndexOf(address);
+      if (idx === -1) {
+        this.openNotification(
+          "Cannot remove hotspot",
+          "Hotspot is not in the list.",
+          "error"
+        );
+      } else {
+        this.hotspotDict.splice(idx, 1);
+        this.inputHotspotAddress = "";
+        this.openNotification(
+          "Succesful",
+          "Hotspot has been removed.",
+          "success"
+        );
+      }
     },
   },
 };
@@ -303,8 +404,16 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#banner {
+  height: 3rem;
+  background: black;
+  color: white;
+  text-align: center;
+  vertical-align: middle;
+  margin: auto;
+  padding: 10px;
 }
 </style>
